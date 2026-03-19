62af902cc98d5b77e6af37a7afe4223e44c24cc7
dr6401 <dr6401@student.uni-lj.si>
2026-03-19 13:52:35 +0100

    Some more upgrades

--- Assets/Resources/Upgrades/1-Silver/DrillDriller.cs
+++ Assets/Resources/Upgrades/1-Silver/DrillDriller.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Augment", menuName = "Augments/Silver/Drill/DrillDriller")]
+[CreateAssetMenu(fileName = "Augment", menuName = "Augments/Drill/DrillDriller")]
 public class DrillDriller : Augment
 {
+    [Header("Augment Specifics")]
+    public int damageIncrease;
     public override void Apply(GameObject player)
     {
-        PlayerStats.Instance.drillDamage += 1;
+        PlayerStats.Instance.drillDamage += damageIncrease;
     }
 }
